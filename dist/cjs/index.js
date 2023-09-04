fc8a8b578d66c73b4f48358c282ebe97c2c062f8
vimala-siravi <vimala1995@gmail.com>
2023-09-04 10:44:18 +0530

    initial commit

---
+++ dist/cjs/index.js
@@ -0,0 +1,2 @@
+"use strict";var e=require("react"),t=require("@mui/material/Button");exports.Button=r=>e.createElement(t,Object.assign({},r),null==r?void 0:r.children);
+//# sourceMappingURL=index.js.map
---
+++ dist/esm/index.js
@@ -0,0 +1,2 @@
+import t from"react";import e from"@mui/material/Button";const r=r=>t.createElement(e,Object.assign({},r),null==r?void 0:r.children);export{r as Button};
+//# sourceMappingURL=index.js.map
